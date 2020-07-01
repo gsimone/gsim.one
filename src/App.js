4eb2eba43de97586d60645fb6cf7f72d3c858c47
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-07-01 16:21:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      className="
+      h-screen 
+      w-screen 
+
+      bg-gray-900 
+      text-white 
+      
+      flex 
+      justify-center 
+      items-center 
+
+      text-6xl
+      font-display
+      font-black
+    "
+    >
+      Hello world.
+    </div>
+  );
+}
+
+export default App;
